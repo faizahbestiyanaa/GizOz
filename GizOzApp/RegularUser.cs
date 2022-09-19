3b20e92dce4e3f3c13f93a809530f1e20ac0c5a4
Banendra17 <muhammadbanendra@mail.ugm.ac.id>
2022-09-19 14:38:45 +0700

    Menambahkan Class>

---
+++ GizOzApp/RegularUser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GizOzApp
+{
+    class RegularUser : User
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string gender { get; set; }
+        public string profession { get; set; }
+        public double height { get; set; }
+        public double weight { get; set; }
+        public string allergy { get; set; }
+
+        public RegularUser(string email, string password, bool loginStatus)
+        {
+            this.email = email;
+            this.password = password;
+            this.loginStatus = loginStatus;
+        }
+
+
+        public override void register()
+        {
+
+        }
+        public override void login()
+        {
+
+        }
+
+        public void updateProfile()
+        {
+
+        }
+
+        public void updateData()
+        {
+
+        }
+    }
+}
---
+++ GizOzApp/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GizOzApp
+{
+    public abstract class User
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+        public bool loginStatus { get; set; }
+
+        public abstract void register();
+        public abstract void login();
+    }
+}
